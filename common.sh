872d936dd55085d2ccc858e8f2eb2ca54cfda79f
Chen Yi <johnny.chenyi@huawei.com>
2019-01-17 17:47:22 +0100

    common: Fix up
    Signed-off-by: Chen Yi <johnny.chenyi@huawei.com>

--- common.sh
+++ common.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
+#
+# author : chen yi <johnny.chenyi@huawei.com>
+# 2018-7-12
+#
+
+# define CI_DIR, CONFIG_DIR, LOG_DIR
+export_ci_dir() {
+	local log_dir_name=log_`date +%F-%H`
+	local bin_dir_name=bin_`date +%F-%H`
+	CI_DIR=$(realpath .)
+
+
+	export CI_DIR
+	export CONFIG_DIR=$CI_DIR/configs
+	export LOG_DIR=$CI_SPACE/log/$log_dir_name
+	export BIN_DIR=$CI_SPACE/bin/$bin_dir_name
+}
+
+export_source_dir() {
+    SOURCE_DIR=$(realpath ..)
+    export SOURCE_DIR
+
+	export HMK_BIN_DIR=$SOURCE_DIR/images
+}
+
+top_dir() {
+    local self test_dir
+
+    self=$(realpath "$0")
+    test_dir=${self%/*}
+    if [[ -d "$test_dir" ]] ; then
+	cd "$test_dir"
+    fi
+}
+
 ### $1 prefix of log file
-function get_log_name()
-{	
-	local  result=$1_`date +%F-%H-%M`.log
-	
+get_log_name(){
+    local  result=$1_`date +%F-%H-%M`.log
+
 	echo $result
 }
 
+mk_dir() {
+	if [ ! -d "$1" ]; then
+		mkdir -p $1
+	fi
+}
